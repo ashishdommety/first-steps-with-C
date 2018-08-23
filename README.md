126fedaca62559454bb0ab0d57c2817225af7bac
unknown <ashishuiux@gmail.com>
2018-08-23 16:01:36 -0700

    add simple unformatted program

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # First Steps with C 
 A simple input/output program with C
+
+## Idea:
+- Ask a series of questions to the user
+- Once you've collected all the answers, display them out in the console with additional text
--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char name[64];
+    char place[64];
+    char animal[64];
+    char thing[64];
+
+    puts("Enter a name: ");
+    fgets(name,64,stdin);
+    puts("Enter a place: ");
+    fgets(place,64,stdin);
+    puts("Enter an animal: ");
+    fgets(animal,64,stdin);
+    puts("Enter a thing: ");
+    fgets(thing,64,stdin);
+
+    printf(" Let me tell you a story! : ");
+    // printf(name);
+    printf("%s flew to %s to find his pet %s that was eating a %s", name, place, animal, thing);
+    /* printf(place);
+    printf(" to find his pet ");
+    printf(animal);
+    printf(" that was eating his ");
+    printf(thing);*/
 }
